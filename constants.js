acdc87e2dc44e16f9570d37ac9f343fec15169f9
Ming <642203604@qq.com>
2020-01-09 18:12:14 +0800

    :beers: Writing code drunkenly

--- constants.js
+++ constants.js
@@ -2,9 +2,10 @@ const constants = {
   USER_CONFIG_NAME: '.collierc',
   PROJECT_CONFIG_NAMES: ['.collie.json', '.collie', 'collie.json', 'collie'],
   DEFAULT_PACKAGES_PATH: 'collie_packages',
-  DEFAULT_REGISTRY: 'http://localhost:26553/',
-  REGISTRY_DATA: '/packages/data.json',
-  PACKAGE_DATA: '/packages/{packageName}/data.json'
+  DEFAULT_LOCAL_PATH: '.collie',
+  DEFAULT_REMOTE_REGISTRY: 'http://localhost:26553/',
+  REGISTRY_DATA: '/packages/info.json',
+  PACKAGE_INFO: '/packages/{packageName}/info.json'
 }
 
 Object.freeze(constants)
--- middleware/registry.js
+++ middleware/registry.js
@@ -1,27 +1,40 @@
+const os = require('os')
+const path = require('path')
 const axios = require('axios')
 
 const constants = require('../constants')
 
 let axiosInstance = null
 
-const registryInterface = {}
-
-registryInterface.axios = axiosInstance
-registryInterface.getPackageData = async (packageName) => {
-  let response = await axiosInstance.get(constants.PACKAGE_DATA.replace('{packageName}', packageName))
+async function getPackageData(packageName) {
+  let response = await axiosInstance.get(constants.PACKAGE_INFO.replace('{packageName}', packageName))
   let packageData = response.data
   return packageData
+}
+// 获取远端的 package 信息
+function getRemotePackageInfo(packageName) {
+  let response = await axiosInstance.get(constants.PACKAGE_INFO.replace('{packageName}', packageName))
+  let packageInfo = response.data
+  return packageInfo
+}
+// 获取本地的 package 信息
+function getLocalPackageData(packageName) {
+  path.join(os.homedir(), constants.USER_CONFIG_NAME)
 }
 
 module.exports = argv => {
   axiosInstance = axios.create({
-    baseURL: argv.$config.getUserConfig('registry') || constants.DEFAULT_REGISTRY,
+    baseURL: argv.$config.getUserConfig('registry') || constants.DEFAULT_REMOTE_REGISTRY,
     // timeout: 60000,
     headers: {
-      'User-Agent': 'collie/0.0.1'
+      'User-Agent': `collie/${require('../package.json').version}`
     }
   })
 
+  const registryInterface = {
+    axios: axiosInstance,
+    getPackageData,
+  }
   Object.freeze(registryInterface)
   argv.$registry = registryInterface
 
