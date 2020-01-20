54e1c19efbaed342acc7a17eef6532cc0dc241b4
Ming <642203604@qq.com>
2020-01-20 15:55:04 +0800

    :recycle: Refactoring code

--- constants.js
+++ constants.js
@@ -1,11 +1,12 @@
 const constants = {
-  USER_CONFIG_NAME: '.collierc',
-  PROJECT_CONFIG_NAMES: ['.collie.json', '.collie', 'collie.json', 'collie'],
-  DEFAULT_PACKAGES_PATH: 'collie_packages',
-  DEFAULT_LOCAL_PATH: '.collie',
-  DEFAULT_REMOTE_REGISTRY: 'http://localhost:26553/',
-  REGISTRY_DATA: '/packages/info.json',
-  PACKAGE_INFO: '/packages/{packageName}/info.json'
+  config: {
+    DEFAULT_USER_CONFIG_NAME: '.collierc',
+    DEFAULT_PROJECT_CONFIG_NAMES: ['.collie.json', '.collie', 'collie.json', 'collie'],
+    DEFAULT_LOCAL: '.collie',
+    DEFAULT_REGISTRY: 'http://localhost:26553/',
+    DEFAULT_PATH: 'collie_packages',
+    DEFAULT_PACKAGES: {}
+  }
 }
 
 Object.freeze(constants)
