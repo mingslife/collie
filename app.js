f9337e346984e10abafd05c069f05dfec40e5c1d
Ming <642203604@qq.com>
2020-01-20 19:23:16 +0800

    :construction: app.js is WIP

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const yargs = require('yargs')
 
-const config = require('./middleware/config')
 const registry = require('./middleware/registry')
 
-yargs
-  .middleware(config)
-  .middleware(registry)
-  .commandDir('commands')
-  .help().argv
+const config = require('./config')()
+const models = require('./models')({
+  localPath: config.localPath,
+  remoteRegistry: config.remoteRegistry
+})
+
+console.log(config.localPath)
+console.log(config.remoteRegistry)
+console.log(config.packagesPath)
+config.projectConfig.putPackage('bootstrap', '4.4.1')
+
+// yargs
+//   // .middleware(config())
+//   .commandDir('commands')
+//   .help().argv
