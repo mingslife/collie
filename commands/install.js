4bd98e3671e52a280cb807e077179671f05a9fd4
Ming <642203604@qq.com>
2020-01-21 16:49:10 +0800

    :boom: Finish developing the install command

--- commands/install.js
+++ commands/install.js
@@ -35,27 +35,21 @@ module.exports = {
   },
   async handler (argv) {
     let info = getInfo(argv.packageName)
-    let infos = argv.context.models.getAllInfos()
-    console.info(infos)
     let package = argv.context.config.projectConfig.getPackage(argv.packageName)
-    if (package) {
-      // ProjectConfig有的，且没装过的，安装
-      let isInstalled = infos.filter(x => x.name == info.name)
-
-      // ProjectConfig有的，且装过的，指定了版本的，安装指定版本，并更新ProjectConfig
-
-      // ProjectConfig有的，且装过的，没指定版本的，升级，并更新ProjectConfig
+    // 这里可能会有下面4种情况
+    // 1 ProjectConfig有的，且没装过的，安装
+    // 2 ProjectConfig有的，且装过的，指定了版本的，安装指定版本，并更新ProjectConfig
+    // 3 ProjectConfig有的，且装过的，没指定版本的，升级，并更新ProjectConfig
+    // 4 ProjectConfig没有的，添加
+    // 为了减少开发者和使用者的心智负担，这里一律采用覆盖安装
+    package = await argv.context.models.getPackage(`${info.repo}/${info.name}`)
+    let release = null
+    if (info.version) {
+      release = package.getRelease(info.version)
     } else {
-      // ProjectConfig没有的，添加
-      package = await argv.context.models.getPackage(`${info.repo}/${info.name}`)
-      let release = null
-      if (info.version) {
-        release = package.getRelease(info.version)
-      } else {
-        release = package.getLatestRelease()
-      }
-      await release.download()
-      argv.context.config.projectConfig.putPackage(argv.packageName, release.version)
+      release = package.getLatestRelease()
     }
+    await release.download(true)
+    argv.context.config.projectConfig.putPackage(argv.packageName, release.version)
   }
 }
